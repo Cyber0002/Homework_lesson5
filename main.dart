8867ed5b5b524d0e982fe6229fdf196d87db76b6
Баймырза <baimyrza@MacBook-Air-Bajmyrza.local>
2023-09-10 17:32:59 +0600

    abc

--- main.dart
+++ main.dart
@@ -1,3 +1,79 @@
-void main(){
-  print('Hellow world');
+
+// import 'dart:io';
+//void main(){
+
+//   //1
+// //  double a = 5;
+// //  double b = 5.126;
+// //  double sum = a + b;
+// //  double minus = a - b;
+// //  double times = a * b;
+// //  double divide = a / b;
+
+// //  print("Сумма: $sum");
+// //  print("Разность: $minus");
+// //  print("Произведение: $times");
+// //  print("Частное: $divide");
+
+//   //2
+//   // int num1 = 5;
+//   // int num2 = 2;
+
+//   // int sum = num1 + num2;
+//   // int minus = num1 - num2;
+//   // int times = 132 * 123;
+//   // double div = 78 / 2;
+//   // double div2 = 78 / 2.5;
+//   // int times2 = 0 * 5;
+
+//   // print("5 + 2 = $sum");
+//   // print("5 - 2 = $minus");
+//   // print("132 * 123 = $times");
+//   // print("78 / 2 = $div");
+//   // print("78 / 2.5 = $div2");
+//   // print("0 * 5 = $times2");
+  
+//   //3
+
+  // int a = 5;
+  // int b = 2;
+
+  // a = a + b;
+  // b = a - b;
+  // a = a - b;
+
+  // print("a = $a, b = $b");
+
+//   // print("b = $b");
+
+//   //4
+
+
+//14
+
+  // List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  
+  // for (int index = 0; index < a.length; index++) {
+  //   if (a[index] < 5) {
+  //     print("Элемент с индексом $index: ${a[index]}");
+  //   }
+  // }
+
+//15
+
+// int a = 10;
+// String res = '';
+// for(var i = 8; i <= a; i++);
+// res += ("*" * 8);
+// res += (' ');
+// print(res);
+
+
+
+void main() {
+
+
+
 }
+
+
